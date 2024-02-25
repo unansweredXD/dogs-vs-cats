e7cfec3672673b958a0ce668841b616fde8d9b8b
unansweredXD <prytov.m4@gmail.com>
2024-02-25 23:35:23 +0700

    * converter + gitignore

--- convert.py
+++ convert.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 
 # Указываем путь к директории
 directory = "PetImages/Cat"
@@ -9,11 +10,16 @@ files = []
 # Добавляем файлы в список
 files += os.listdir(directory)
 
-import pathlib
-
 # На Windows путь будет что-то вроде:
 # D:\\user\\images
+os.mkdir('train_all')
+
 path = pathlib.Path("PetImages/Dog")
 for i, path in enumerate(path.glob('*.jpg')):
     new_name = 'dog_' + str(i) + '.jpg'
-    path.rename(new_name)
+    path.rename('train_all/' + new_name)
+
+path = pathlib.Path("PetImages/Cat")
+for i, path in enumerate(path.glob('*.jpg')):
+    new_name = 'cat_' + str(i) + '.jpg'
+    path.rename('train_all/' + new_name)
